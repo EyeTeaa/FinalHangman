d8f8ef8a63b48f6a317cb3b50e13611efcee4abd
aithy ho <91100494@g.mvusd.net>
2021-12-10 08:54:20 -0800

    updated variables and added new methods in class hangman

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,10 +1,13 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Hangman
 {
-    private String[] listOfWords = {"apple", "banana", "cat", "dog", "elephant"};
-    private int randNum = (int) (Math.random() * (listOfWords.length-1));
-    private String randWord = listOfWords[randNum];
+    Scanner scan = new Scanner(System.in);
+    private String[] listOfAnimals = {"apple", "banana", "cat", "dog", "elephant"};
+    private int randNum = (int) (Math.random() * (listOfAnimals.length-1));
+    private String randWord = listOfAnimals[randNum];
+    private String input = "";
 
     private String[] wordDivided = new String[randWord.length()];
     //constructors
@@ -23,6 +26,48 @@ public class Hangman
         {
             wordDivided[v] = word.substring(v,v+1);
             System.out.println(wordDivided[v]);
+        }
+    }
+
+    public void generateSpaces()
+    {
+
+    }
+
+    // a list of all the prompts and outputs
+    public void listOfPrompts(int promptNum)
+    {
+        if (promptNum == 1 )
+        {
+            System.out.println("An animal has been selected");
+        }
+    }
+
+    //validating the input from the user
+    public void validateInput(String input)
+    {
+        if (input.equals(randWord))
+        {
+            System.out.println("same word");
+        }
+        else if (input.length() == 1)
+        {
+            for(int i = 0 ; i < wordDivided.length; i++)
+            {
+                if (wordDivided[i].equals(input))
+                {
+                    System.out.println("good");
+                    break;
+                }
+                else
+                {
+                    System.out.println("not good");
+                }
+            }
+        }
+        else
+        {
+            System.out.println("You have guessed the word wrong ");
         }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,7 @@ public class Main
         Hangman yea = new Hangman();
         yea.populateLettersOfWord(yea.getRandWord());
         System.out.println(yea.getWordDivided());
-//        // generate random numbers within 1 to 10
-//        for (int i = 0; i < 10; i++) {
-//            int rand = (int) (Math.random() * 6) ;
-//            System.out.println(rand);
-//        }
 
-//        String[] listOfWords = {"apple", "banana", "cat", "dog", "elephant"};
-//         int randNum = (int) (Math.random() * listOfWords.length);
-//         for (int v  = 0 ; v < 10 ; v++){
-//             randNum = (int) (Math.random() * listOfWords.length);
-//             String randWord = listOfWords[randNum];
-//             System.out.println(randWord);
-//             System.out.println(randWord.length());
          }
 
     }
